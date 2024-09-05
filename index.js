aa9f06ed57cf6a2c64a2597cfcb24e9cddb91c8b
UjwalShakhakarmi <ujwalshakha@gmail.com>
2024-09-05 21:10:06 +0545

    second Commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function createTextElement(text) {
     },
   };
 }
-function render(element, container) {
+function createDom(fiber) {
   //TODO create dom nodes
   const dom =
     element.type == "TEXT_ELEMENT"
@@ -46,9 +46,137 @@ function render(element, container) {
     .forEach((name) => {
       dom[name] = element.props[name];
     });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+  return dom;
 }
+function updateDom(dom, prevProps, nextProps) {
+  // TODO
+}
+function commitRoot() {
+  //we recursively append all the nodes to the dom.
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom);
+  }
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    //We also add the alternate property to every fiber. This property is a link to the old fiber, the fiber that we committed to the DOM in the previous commit phase.
+    alternate: currentRoot,
+  };
+  deletions = [];
+  nextUnitOfWork = wipRoot;
+}
+let nextUnitOfWork = null;
+let currentRoot = null;
+let wipRoot = null;
+let deletions = [];
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+  // You can think of requestIdleCallback as a setTimeout,
+  requestIdleCallback(workLoop);
+}
+
+function performUnitOfWork(fiber) {
+  //The next fiber to work on could be:
+  //The current fiber's child (if it has any).
+  //If no child, then it looks for the fiber's sibling.
+  //If neither child nor sibling is available, it moves back up the tree to the parent to look for the parent's sibling.
+  // Create a DOM node (like <div>, <a>, etc.) if it doesn't exist yet
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  // Get the children elements from the fiber's props
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
+  // Return the next unit of work (child, sibling, or move up the tree)
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+function reconcileChildren(wipFiber, elements) {
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+  // Loop through each element and create a new fiber for each
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    const sameType = oldFiber && element && element.type == oldFiber.type;
+    if (sameType) {
+      //update the node
+      //we create a new fiber keeping the DOM node from the old fiber and the props from the element.
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      //TODO add this node
+      const newFiber = {
+        type: element.type,
+        props: element.props,
+        parent: wipFiber,
+        dom: null, // DOM will be created later
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    if (oldFiber && !sameType) {
+      //delete oldFiber's node
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    // Set the first child of the fiber
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      // Set the sibling relationship for other children
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
 const Didact = {
   createElement,
   render,
